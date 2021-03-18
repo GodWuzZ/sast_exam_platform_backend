edca7b17692a299027e441dcebc5acad28ffe47f
GodWuzZ <shermanfdy@gmail.com>
2021-03-18 21:00:44 +0800

    Inheritance and improvement

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Work\WorkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('work.feng')->any('/work',[WorkController::class,'GodWu']);
